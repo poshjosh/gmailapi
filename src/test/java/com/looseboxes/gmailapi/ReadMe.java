0a4853791edaa2225bb0bfb8ff37e915771ed341
poshjosh <posh.bc@gmail.com>
2021-02-14 22:05:02 +0100

    Use utility methods for creating Messages directly from content

--- src/test/java/com/looseboxes/gmailapi/ReadMe.java
+++ src/test/java/com/looseboxes/gmailapi/ReadMe.java
@@ -1,9 +1,8 @@
 package com.looseboxes.gmailapi;
 
 import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.model.Message;
 import java.util.Properties;
-import javax.mail.internet.MimeMessage;
-import org.junit.jupiter.api.Test;
 
 /**
  * @author chinomso ikwuagwu
@@ -12,26 +11,29 @@ public class ReadMe {
     
     public static void main(String... args) {
         
-        final String appName = "my app";
+        final String appName = "MyApp";
+        
         Properties props = new Properties();
         props.setProperty(GmailAccessProperties.ACCESS_TOKEN_SCOPES, "https://www.googleapis.com/auth/gmail.send");
+
         props.setProperty(GmailAccessProperties.CREDENTIALS_FILE, "[PATH TO THE JSON FILE YOU DOWNLOADED FROM GOOGLE]");
+
         props.setProperty(GmailAccessProperties.TOKENS_DIRECTORY, "[PATH TO A DIRECTORY WHERE TOKENS WILL BE SAVED]");
         
         try{
 
-            final String from = "looseboxes@gmail.com";
+            final String from = "chinomsoikwuagwu@herobids.com";
             final String to = "posh.bc@gmail.com";
             final String subject = "Subject 1";
             final String content = "Message 1";
                    
-            final MimeMessage mimeMessage = MessageUtil.createEmail(to, from, subject, content);
+            final Message message = MessageUtil.createMessage(to, from, subject, content);
 
             Gmail gmail = new GmailProviderImpl(appName, new GmailConfigImpl(props)).get();
             
             // me is a keyword used to refer to the current user
             //
-            gmail.users().messages().send("me", MessageUtil.fromMimeMessage(mimeMessage));
+            gmail.users().messages().send("me", message);
             
         }catch(Exception e) {
         
